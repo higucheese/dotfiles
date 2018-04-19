45d8c2c7172cb2b46e175f921951874e91b1806e
higucheese <t.higuchi.eeic@gmail.com>
2018-04-19 15:00:10 +0900

    add .bashrc

---
+++ install.sh
@@ -0,0 +1,15 @@
+/bin/sh
+
+dirs=(".emacs.d" ".config/nvim")
+
+for dir in "${dirs[@]}"
+do
+    mkdir -p "${HOME}/${dir}"
+done
+
+ln -s init.el ${HOME}/.emacs.d
+ln -s init.vim ${HOME}/.config/nvim
+ln -s .tmux.conf ${HOME}/.tmux.conf
+ln -s .zshrc ${HOME}/.zshrc
+ln -s .zshrc.linux ${HOME}/.zshrc.linux
+ln -s .gitconfig ${HOME}/.gitconfig
