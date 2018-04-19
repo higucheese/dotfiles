154a7a13f1bf917aa630a91aaffc6ec2f36c2613
thiguchi <thiguchi@eidos.ic.i.u-tokyo.ac.jp>
2018-04-19 17:23:20 +0900

    add support for normal vim

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dirs=(".emacs.d" ".config/nvim" ".config/nvim/colors")
+dirs=(".emacs.d" ".config/nvim" ".vim/colors" ".config/nvim/colors")
 
 for dir in "${dirs[@]}"
 do
@@ -9,9 +9,11 @@ done
 
 dotfiles="${HOME}/dotfiles"
 ln -si ${dotfiles}/init.el ${HOME}/.emacs.d
-ln -si ${dotfiles}/init.vim ${HOME}/.config/nvim
 ln -si ${dotfiles}/.tmux.conf ${HOME}/.tmux.conf
+ln -si ${dotfiles}/init.vim ${HOME}/.config/nvim
+ln -si ${dotfiles}/.vimrc ${HOME}/.vimrc
 ln -si ${dotfiles}/.zshrc ${HOME}/.zshrc
 ln -si ${dotfiles}/.zshrc.linux ${HOME}/.zshrc.linux
 ln -si ${dotfiles}/.bashrc ${HOME}/.bashrc
 ln -si ${dotfiles}/vim/vim-hybrid/colors/hybrid.vim ${HOME}/.config/nvim/colors/hybrid.vim
+ln -si ${dotfiles}/vim/vim-hybrid/colors/hybrid.vim ${HOME}/.vim/colors/hybrid.vim
